0c58402d3e6de5b3f487ca0dc98df66d50781890
Travis Collins <travisfcollins@gmail.com>
2025-12-10 16:11:44 -0500

    Add SoC SSH initial strat
    Signed-off-by: Travis Collins <travisfcollins@gmail.com>

---
+++ adi_lg_plugins/strategies/bootfpgasocssh.py
@@ -0,0 +1,150 @@
+import enum
+import time
+
+import attr
+
+from labgrid.step import step
+from labgrid.strategy import Strategy, StrategyError, never_retry
+from labgrid.factory import target_factory
+
+class Status(enum.Enum):
+    unknown = 0
+    powered_off = 1
+    booting = 2
+    booted = 3
+    update_boot_files = 4
+    reboot = 5
+    booting_new = 6
+    shell = 7
+    soft_off = 8
+
+@target_factory.reg_driver
+@attr.s(eq=False)
+class BootFPGASoCSSH(Strategy):
+    bindings = {
+        "power": {"PowerProtocol", None},
+        "shell": "ADIShellDriver",
+        "ssh": "SSHDriver",
+    }
+
+    status = attr.ib(default=Status.unknown)
+    hostname = attr.ib(default="analog")
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        # self.hostname =
+        print(f"Hostname set to: {self.hostname}")
+        self.logger.info("BootFPGASoCSSH strategy initialized")
+
+    @never_retry
+    @step()
+    def transition(self, status, *, step):
+        if not isinstance(status, Status):
+            status = Status[status]
+
+        self.logger.debug(f"Transitioning to {status} (Existing status: {self.status}) {status == Status.shell}")
+
+        if status == Status.unknown:
+            raise StrategyError(f"can not transition to {status}")
+        elif status == self.status:
+            step.skip("nothing to do")
+            return  # nothing to do
+        elif status == Status.powered_off:
+            self.target.deactivate(self.shell)
+            if self.power:
+                self.target.activate(self.power)
+                self.power.off()
+            self.logger.debug("DEBUG Powered off")
+        elif status == Status.booting:
+            self.transition(Status.powered_off)
+            if self.power:
+                self.target.activate(self.power)
+                time.sleep(5)
+                self.power.on()
+            self.logger.debug("DEBUG Booting...")
+        elif status == Status.booted:
+            self.transition(Status.booting)
+            if self.power:
+                self.shell.bypass_login = True
+                self.target.activate(self.shell)
+                # Check kernel start
+                self.shell.console.expect("Linux", timeout=30)
+                # Check device prompt
+                self.shell.console.expect(self.hostname)  # Adjust prompt as needed
+                self.shell.bypass_login = False
+                self.target.deactivate(self.shell)
+            self.logger.debug("DEBUG Booted")
+
+        elif status == Status.update_boot_files:
+            self.transition(Status.booted)
+
+            # Get IP address from shell
+            self.target.activate(self.shell)
+            addresses = self.shell.get_ip_addresses("eth0")
+            assert addresses, "No IP address found on eth0"
+            ip = str(addresses[0].ip)
+            self.target.deactivate(self.shell)
+
+            if self.ssh.networkservice.address != ip:
+                self.logger.warning(f"IP address mismatch between ShellDriver ({ip}) and SSHDriver ({self.ssh.networkservice.address})")
+                self.logger.warning("Updating SSHDriver IP address to match ShellDriver")
+                self.ssh.networkservice.address = ip # Update
+
+            self.target.activate(self.ssh)
+
+            self.ssh.put("/tmp/testfile.txt", "/tmp/testfile.txt")
+
+            self.target.deactivate(self.ssh)
+
+            # Use SSH to update boot files here
+            self.logger.debug("DEBUG Boot files updated via SSH")
+
+
+        elif status == Status.reboot:
+            self.transition(Status.update_boot_files)
+            self.target.activate(self.shell)
+            try:
+                self.shell.run("reboot")
+            except Exception as e:
+                self.logger.info(f"DEBUG Reboot command exception (expected): {e}")
+            self.target.deactivate(self.shell)
+            self.logger.debug("DEBUG Rebooted")
+
+        elif status == Status.booting_new:
+            self.transition(Status.reboot)
+
+            self.shell.bypass_login = True
+            self.target.activate(self.shell)
+            # Check kernel start
+            self.shell.console.expect("Linux", timeout=30)
+            # Check device prompt
+            self.logger.info(f"DEBUG Expecting hostname prompt: {self.hostname}")
+            self.shell.console.expect(self.hostname)  # Adjust prompt as needed
+            self.target.deactivate(self.shell)
+            self.shell.bypass_login = False
+            self.logger.debug("DEBUG Booting new...")
+
+        elif status == Status.shell:
+            self.transition(Status.booting_new)
+            self.target.activate(self.shell)
+            # Post boot stuff...
+        elif status == Status.soft_off:
+            self.transition(Status.shell)
+
+            # try:
+            #     self.shell.run("poweroff")
+            #     self.shell.console.expect("Power down", timeout=30)
+            #     self.target.deactivate(self.shell)
+            #     time.sleep(10)
+            # except Exception as e:
+            #     self.logger.debug(f"DEBUG Soft off failed: {e}")
+            #     time.sleep(5)
+            #     self.target.deactivate(self.shell)
+            # self.target.activate(self.power)
+            # self.power.off()
+            self.logger.debug("DEBUG Soft powered off")
+        else:
+            raise StrategyError(
+                f"no transition found from {self.status} to {status}"
+            )
+        self.status = status
