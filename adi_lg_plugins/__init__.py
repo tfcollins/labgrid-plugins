627dcb9f917fd126a046af345671f83be3062e2c
Travis F. Collins <travis.collins@analog.com>
2025-12-10 09:21:11 -0500

    Add initial plugin to support vesync outlets
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

---
+++ adi_lg_plugins/__init__.py
@@ -0,0 +1,5 @@
+"""
+Labgrid plugin package for Analog Devices, Inc. (ADI) specific plugins.
+"""
+
+__version__ = "0.1.0"
---
+++ adi_lg_plugins/drivers/vesyncdriver.py
@@ -0,0 +1,111 @@
+"""
+Example driver for controlling a network power switch.
+
+This demonstrates how to create a driver in a labgrid plugin.
+"""
+
+# import attr
+
+# from labgrid.factory import target_factory
+# from labgrid.driver import Driver
+# from labgrid.protocol import PowerProtocol
+# from labgrid.step import step
+
+
+
+import attr
+import time
+
+# from labgrid.driver import Driver
+from labgrid.driver.common import Driver
+
+# from labgrid.driver.mixin.powerresetmixin import PowerResetMixin
+# from labgrid.driver.protocol.powerprotocol import PowerProtocol
+from labgrid.driver.powerdriver import PowerResetMixin
+from labgrid.protocol import PowerProtocol
+from labgrid.factory import target_factory
+
+from labgrid.step import step
+# import logging
+
+from pyvesync import VeSync
+
+@target_factory.reg_driver
+@attr.s(eq=False)
+class VesyncPowerDriver(Driver, PowerResetMixin, PowerProtocol):
+    """VesyncPowerDriver - Driver using a Vesync Smart Outlet
+    to control a target's power - https://github.com/webdjoe/pyvesync.
+    Uses pyvesync tool to control the outlet."""
+
+    bindings = {"vesync_outlet": {"VesyncOutlet"}}
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        # self.logger = logging.getLogger(f"{self}")
+        # self.logger.debug("Logging into VeSync account")
+        self.pdu_dev = VeSync(self.vesync_outlet.username, self.vesync_outlet.password)
+        self.pdu_dev.login()
+        self.pdu_dev.update()
+        self.outlets = []
+        if not self.pdu_dev.outlets:
+            raise Exception("No VeSync outlets found for this account")
+        # Check and store all outlets
+        self._known_outlets = [o.device_name for o in self.pdu_dev.outlets]
+        outlet_names = self.vesync_outlet.outlet_names.split(",")
+        assert len(outlet_names) > 0, "No outlet names provided"
+        for name in outlet_names:
+            outlet = self._get_outlet_vesync(name.strip())
+            self.outlets.append(outlet)
+            self.logger.debug(f"Using VeSync outlet: {outlet.device_name}")
+
+    def _get_outlet_vesync(self, name):
+        """Get VeSync outlet by name or index"""
+        if isinstance(name, str):
+            for o in self.pdu_dev.outlets:
+                if o.device_name == name:
+                    return o
+            raise Exception(
+                f"Outlet {name} not found" + f" (known outlets: {self._known_outlets})"
+            )
+        elif isinstance(name, int):
+            if name < 0 or name >= len(self.pdu_dev.outlets):
+                raise Exception(f"Outlet index {name} out of range")
+            return self.pdu_dev.outlets[name]
+        else:
+            raise Exception("Outlet must be a string or integer")
+
+    @Driver.check_active
+    @step()
+    def on(self):
+        for outlet in self.outlets:
+            outlet.turn_on()
+        self.logger.debug("Powered ON via Vesync outlet")
+
+    @Driver.check_active
+    @step()
+    def off(self):
+        for outlet in self.outlets:
+            outlet.turn_off()
+        self.logger.debug("Powered OFF via Vesync outlet")
+
+    @Driver.check_active
+    @step()
+    def reset(self):
+        self.off()
+        self.logger.debug(
+            "Waiting %.1f seconds before powering ON", self.vesync_outlet.delay
+        )
+        time.sleep(self.vesync_outlet.delay)
+        self.on()
+
+    @Driver.check_active
+    @step()
+    def cycle(self):
+        self.off()
+        time.sleep(self.vesync_outlet.delay)
+        self.on()
+
+    @Driver.check_active
+    @step()
+    def get(self):
+        return all(outlet.is_on for outlet in self.outlets)
