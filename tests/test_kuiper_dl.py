0ea296ddfceac639de0ab180226681f662444144
Travis F. Collins <travis.collins@analog.com>
2025-12-16 11:58:45 -0500

    Add updates to pyproject.toml for new tool features
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- tests/test_kuiper_dl.py
+++ tests/test_kuiper_dl.py
@@ -1,5 +1,24 @@
+import os
+import json
+
+
 def test_kuiper_dl(target):
     kuiper = target.get_driver("KuiperDLDriver")
     kuiper.download_release()
 
-    kuiper.get_boot_files_from_release()
+    # Verify img file exists
+    path = kuiper.kuiper_resource.cache_path
+    cache_file = os.path.join(path, kuiper.cache_datafile)
+    assert os.path.isfile(cache_file)
+
+    # Load json cache_file
+    with open(cache_file, "r") as f:
+        data = json.load(f)
+
+    release_path = data[kuiper.kuiper_resource.release_version]["image_path"]
+    assert os.path.isfile(release_path)
+
+    files = kuiper.get_boot_files_from_release()
+
+    for file in files:
+        assert os.path.isfile(file)
