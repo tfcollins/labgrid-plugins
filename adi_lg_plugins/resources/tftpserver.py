7d3f1c3719335511458b7d083bfb211ab0d83035
Travis Collins <travisfcollins@gmail.com>
2026-01-31 12:03:44 -0500

    Add TFTP implementations
    Signed-off-by: Travis Collins <travisfcollins@gmail.com>

--- adi_lg_plugins/resources/tftpserver.py
+++ adi_lg_plugins/resources/tftpserver.py
@@ -1,19 +1,23 @@
 import socket
+
 import attr
 from labgrid.factory import target_factory
 from labgrid.resource.common import Resource
+
 
 @target_factory.reg_resource
 @attr.s(eq=False)
 class TFTPServerResource(Resource):
     """Resource to configure or discover the TFTP server address."""
     address = attr.ib(default="auto", validator=attr.validators.instance_of(str))
+    port = attr.ib(default=3069, validator=attr.validators.instance_of(int))
+    root = attr.ib(default="/var/lib/tftpboot", validator=attr.validators.instance_of(str))
 
     def get_ip(self):
         """Returns the configured IP or discovers it if set to 'auto'."""
         if self.address and self.address != "auto":
             return self.address
-        
+
         # Auto-discovery logic (formerly get_local_ip)
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
